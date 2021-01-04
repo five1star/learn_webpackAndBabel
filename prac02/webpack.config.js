db8ebf852899fb5997f735c5eb8d1aabb8635255
five1star <five1star0420@gmail.com>
2021-01-04 19:36:26 +0900

    learn webpack loader plugins

---
+++ prac02/webpack.config.js
@@ -0,0 +1,30 @@
+const { userInfo } = require("os")
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: "url-loader",
+        options: {
+          filename: "[name].[ext]?[hash]",
+          publicPath: "./dist",
+          limit: 10000,
+        },
+      },
+    ],
+  },
+}
